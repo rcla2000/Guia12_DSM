91f3014af2486cef834d17a12bef4d892db1efab
Roberto Carlos López Abarca <rcla200031@gmail.com>
2023-04-29 18:38:57 -0600

    Initial commit

---
+++ app/src/main/java/edu/udb/appcalculadoramvc/CalculatorModel.kt
@@ -0,0 +1,20 @@
+package edu.udb.appcalculadoramvc
+
+class CalculatorModel {
+
+    fun add(num1: Double, num2: Double): Double {
+        return num1 + num2
+    }
+
+    fun subtract(num1: Double, num2: Double): Double {
+        return num1 - num2
+    }
+
+    fun multiply(num1: Double, num2: Double): Double {
+        return num1 * num2
+    }
+
+    fun divide(num1: Double, num2: Double): Double {
+        return num1 / num2
+    }
+}
